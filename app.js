d0ae72e1bc39e4ebcf20175a813078586907b59b
codefeak <codefever17@gmail.com>
2018-07-24 14:24:25 +0300

    added gitignore & http-errors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 const data = require('./public/data.js');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
+const createError = require('http-errors');
 const fs = require('fs');
 
 
@@ -13,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public/assets'));
 app.use(express.static('views'));
 app.use(fileUpload());
+
+
 
 app.get('/', (req, res)=>{
     res.render('index', data);
@@ -68,6 +71,20 @@ app.delete('/indv-files/:id', (req,res)=>{
         res.send('One information Deleted');
     }
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 app.listen(port, ()=>{
     console.log(`Server stated on port ${port}`)
